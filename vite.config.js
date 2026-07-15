6a590021f4e936e90507690ebfdce1fc08b431ea
ShreyaRahulKamble <shreyark1121@gmail.com>
2026-07-15 23:04:44 +0530

    Remove trailing slash from base URL in config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/shraddha-s-creation/',
+  base: '/shraddha-s-creation',
   plugins: [react()],
   build: { outDir: 'dist' }
 })
